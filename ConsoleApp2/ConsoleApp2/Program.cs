a31f300550388095a9158125184581d0ce2ce658
Mahan-Manashtorm <mahan.manashtorm@gmail.com>
2023-04-12 03:54:15 +0300

    lab7

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using yo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new FlyweightFactory(
+            new Mushroom { Name = "Portobello", Size = 2 },
+            new Mushroom { Name = "Chanterelle", Size = 32 },
+            new Mushroom { Name = "Truffle", Size = 15 }
+        );
+        factory.listFlyweights();
+
+        addCarToPoliceDatabase(factory, new Mushroom
+        {
+            Name = "Portobello",
+            Size = 22
+        });
+
+        addCarToPoliceDatabase(factory, new Mushroom
+        {
+            Name = "Toadstool",
+            Size = 11
+        });
+
+        factory.listFlyweights();
+        addGrow(factory, 10);
+        factory.listFlyweights();
+        Console.ReadLine();
+    }
+
+    public static void addCarToPoliceDatabase(FlyweightFactory factory, Mushroom mushroom)
+    {
+        Console.WriteLine("\nClient: Adding a Mushroom to database.");
+
+        var flyweight = factory.GetFlyweight(new Mushroom
+        {
+            Name = mushroom.Name,
+            Size = mushroom.Size,
+        });
+        flyweight.Operation(mushroom);
+    }
+
+    public static void addGrow(FlyweightFactory factory, int time)
+    {
+        Console.WriteLine("\nClient: Adding a Mushroom to database.");
+
+
+        for (int i = 0; i < time; i++)
+        {
+            foreach (var flyweight in factory.GetlistFlyweights())
+            {
+                flyweight.MushroomGrow();
+            }
+        }
+    }
+
+}
---
+++ ConsoleApp2/da/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace da
+{
+    public class Flyweight
+    {
+        private Mushroom _sharedState;
+        public Flyweight(Mushroom mushroom)
+        {
+            this._sharedState = mushroom;
+        }
+        public void MushroomGrow()
+        {
+            Random number = new Random();
+            this._sharedState.Size += number.Next(1, 5);
+        }
+
+        public int PrintGrow()
+        {
+            return this._sharedState.Size;
+        }
+        public void Operation(Mushroom uniqueState)
+        {
+            string s = JsonConvert.SerializeObject(this._sharedState);
+            string u = JsonConvert.SerializeObject(uniqueState);
+            Console.WriteLine($"Flyweight: Displaying shared {s} and unique {u} state.");
+        }
+    }
+    public class FlyweightFactory
+    {
+        private List<Tuple<Flyweight, string>> flyweights = new List<Tuple<Flyweight, string>>();
+
+        public FlyweightFactory(params Mushroom[] args)
+        {
+            foreach (var elem in args)
+            {
+                flyweights.Add(new Tuple<Flyweight, string>(new Flyweight(elem), this.getKey(elem)));
+            }
+        }
+        public string getKey(Mushroom key)
+        {
+            List<string> elements = new List<string>();
+            elements.Add(key.Name);
+            elements.Sort();
+            return string.Join("_", elements);
+        }
+        public Flyweight GetFlyweight(Mushroom sharedState)
+        {
+            string key = this.getKey(sharedState);
+
+            if (flyweights.Where(t => t.Item2 == key).Count() == 0)
+            {
+                Console.WriteLine("FlyweightFactory: Can't find a flyweight, creating new one.");
+                this.flyweights.Add(new Tuple<Flyweight, string>(new Flyweight(sharedState), key));
+            }
+            else
+            {
+                Console.WriteLine("FlyweightFactory: Reusing existing flyweight.");
+            }
+            return this.flyweights.Where(t => t.Item2 == key).FirstOrDefault().Item1;
+        }
+        public void listFlyweights()
+        {
+            var count = flyweights.Count;
+            Console.WriteLine($"\nFlyweightFactory: I have {count} flyweights:");
+            foreach (var flyweight in flyweights)
+            {
+                Console.WriteLine(flyweight.Item2 + " | Size: " + flyweight.Item1.PrintGrow());
+            }
+        }
+        public List<Flyweight> GetlistFlyweights()
+        {
+            var list = new List<Flyweight>();
+
+            foreach (var flyweight in flyweights)
+            {
+                list.Add(flyweight.Item1);
+            }
+
+            return list;
+        }
+    }
+    public class Mushroom
+    {
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+    }
+}
