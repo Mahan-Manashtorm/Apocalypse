ec088d29a4a9c7b5a5df2def156f2a0ee35b803d
Mahan-Manashtorm <mahan.manashtorm@gmail.com>
2023-04-12 03:53:12 +0300

    lab5

---
+++ laba5/da/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace da
+{
+    public class Phone
+    {
+        public string Screen { get; set; }
+        public string Battery { get; set; }
+        public string Camera { get; set; }
+        public string Processor { get; set; }
+
+        public Phone ShallowCopy()
+        {
+            return (Phone)this.MemberwiseClone();
+        }
+
+        public Phone DeepCopy()
+        {
+            Phone clone = (Phone)this.MemberwiseClone();
+            clone.Battery = String.Copy(Battery);
+            clone.Camera = String.Copy(Camera);
+            clone.Processor = String.Copy(Processor);
+            return clone;
+        }
+        public void DisplayValues()
+        {
+            Console.WriteLine("      Screen: {0:s}, Battery: {1:s}, Camera: {2:s}, Processor: {3:s}",
+                Screen, Battery, Camera, Processor);
+        }
+    } 
+}
---
+++ laba5/laba5/Program.cs
@@ -0,0 +1,28 @@
+﻿using da;
+
+Phone p1 = new Phone();
+p1.Screen = "6.1 inch Super Retina XDR display";
+p1.Battery = "Built-in rechargeable lithium-ion battery";
+p1.Camera = "Dual-camera system with 12MP Ultra Wide and Wide cameras";
+p1.Processor = "A15 Bionic chip with Neural Engine";
+Phone p2 = p1.ShallowCopy();
+Phone p3 = p1.DeepCopy();
+Console.WriteLine("Original values of p1, p2, p3:");
+Console.WriteLine("   p1 instance values: ");
+p1.DisplayValues();
+Console.WriteLine("   p2 instance values:");
+p2.DisplayValues();
+Console.WriteLine("   p3 instance values:");
+p3.DisplayValues();
+p1.Screen = "6.7-inch Dynamic AMOLED 2X";
+p1.Battery = "5000mAh (typical) battery capacity";
+p1.Camera = "Triple Rear Camera: 108MP Wide-angle Camera, 12MP Ultra Wide Camera, 10MP Telephoto Camera";
+p1.Processor = "Exynos 2200 with integrated AMD GPU";
+Console.WriteLine("Values of p1, p2 and p3 after changes to p1:");
+Console.WriteLine("   p1 instance values: ");
+p1.DisplayValues();
+Console.WriteLine("   p2 instance values (reference values have changed):");
+p2.DisplayValues();
+Console.WriteLine("   p3 instance values (everything was kept the same):");
+p3.DisplayValues();
+      
