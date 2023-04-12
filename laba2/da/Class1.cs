1f92ead21b2c63640fd07d4ef4e61e9f213f3baa
Mahan-Manashtorm <mahan.manashtorm@gmail.com>
2023-04-12 03:49:45 +0300

    lab2

---
+++ laba2/da/Class1.cs
@@ -0,0 +1,65 @@
+﻿namespace da
+{
+    public class House
+    {
+        public string Material { get; set; }
+
+        public override string ToString()
+        {
+            return $"Material: {Material}";
+        }
+    }
+    public interface IHouseCompany
+    {
+        void SetMaterial();
+        void SetnameCompan();
+        string nameCompany { get; set; }
+        House GetHouse();
+    }
+    public class WoodHouseCompany : IHouseCompany
+    {
+        private readonly House _house = new House();
+        public string nameCompany { get; set; }
+        public void SetMaterial()
+        {
+            _house.Material = "Wood";
+        }
+        public void SetnameCompan()
+        {
+            nameCompany = "Sus";
+        }
+        public House GetHouse()
+        {
+            return _house;
+        }
+    }
+    public class BrickHouseCompany : IHouseCompany
+    {
+        private readonly House _house = new House();
+        public string nameCompany { get; set; }
+        public void SetMaterial()
+        {
+            _house.Material = "Brick";
+        }
+        public void SetnameCompan()
+        {
+            nameCompany = "Bus";
+        }
+        public House GetHouse()
+        {
+            return _house;
+        }
+    }
+    public class HouseCompany
+    {
+        public House Construct(IHouseCompany builder)
+        {
+
+            builder.SetMaterial();
+            builder.SetnameCompan();
+            return builder.GetHouse();
+        }
+    }
+
+
+}
---
+++ laba2/laba2/Program.cs
@@ -0,0 +1,11 @@
+﻿using da;
+
+HouseCompany builder = new HouseCompany();
+IHouseCompany woodHouseBuilder = new WoodHouseCompany();
+IHouseCompany brickHouseBuilder = new BrickHouseCompany();
+
+House woodHouse = builder.Construct(woodHouseBuilder);
+House brickHouse = builder.Construct(brickHouseBuilder);
+
+Console.WriteLine(woodHouse.ToString() + "; Company - " + woodHouseBuilder.nameCompany);
+Console.WriteLine(brickHouse.ToString() + "; Company - " + brickHouseBuilder.nameCompany);
