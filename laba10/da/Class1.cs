dc7216c0a25e79c880e0150893596aca929bf5bc
Mahan-Manashtorm <mahan.manashtorm@gmail.com>
2023-04-12 04:05:55 +0300

    lab10

---
+++ laba10/da/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace da
+{
+    public interface IBankConsultant
+    {
+        string GetAdvice(string question);
+    }
+
+    public class RealSubject : IBankConsultant
+    {
+        public string GetAdvice(string question)
+        {
+            return "Наразi ми не можемо вiдповiсти на ваше питання " + question;
+        }
+    }
+    public class Proxy : IBankConsultant
+    {
+        private RealSubject _realSubject;
+
+        public Proxy(RealSubject realSubject)
+        {
+            this._realSubject = realSubject;
+        }
+
+        private List<string> _allowedQuestions = new List<string> { "Запитання 1", "Запитання 2", "Запитання 3" };
+
+        public string GetAdvice(string question)
+        {
+            if (_allowedQuestions.Contains(question))
+            {
+                return _realSubject.GetAdvice(question);
+            }
+            else
+            {
+                return "Консультант не может вiдповiсти на запитання";
+            }
+        }
+    }
+    public class Client
+    {
+        public void ClientCode(IBankConsultant subject)
+        {
+            string question = Console.ReadLine();
+
+            Console.WriteLine(subject.GetAdvice(question));
+        }
+    }
+}
---
+++ laba10/laba10/Program.cs
@@ -0,0 +1,22 @@
+﻿using da;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            RealSubject realSubject = new RealSubject();
+            Console.WriteLine("Client: Запитання до проксi працiвника");
+            Proxy proxy = new Proxy(realSubject);
+            client.ClientCode(proxy);
+            Console.ReadLine();
+        }
+    }
+}
