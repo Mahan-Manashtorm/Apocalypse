cfd7cd39f1e93f0eec9b03b2d9dc082701142a3c
Mahan-Manashtorm <mahan.manashtorm@gmail.com>
2023-04-12 03:53:40 +0300

    lab6

---
+++ laba6/da/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace da
+{
+    interface ISetAdapter<T>
+    {
+        ISet<T> ToSet(T[] array); 
+        T[] ToArray(ISet<T> set); 
+    }
+    public class SetAdapter<T> : ISetAdapter<T>
+    {
+        public ISet<T> ToSet(T[] array)
+        {
+            ISet<T> set = new HashSet<T>(array); 
+            return set;
+        }
+
+        public T[] ToArray(ISet<T> set)
+        {
+            T[] array = new T[set.Count]; 
+            set.CopyTo(array, 0); 
+            return array;
+        }
+    }
+}
---
+++ laba6/laba6/Program.cs
@@ -0,0 +1,24 @@
+﻿using da;
+
+SetAdapter<int> adapter = new SetAdapter<int>(); 
+int[] array = { 1, 2, 3, 4, 5 };
+
+ISet<int> set = adapter.ToSet(array);
+Console.WriteLine("Множество: ");
+foreach (int item in set)
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine();
+set.Add(6);
+set.Add(7);
+
+int[] newArray = adapter.ToArray(set);
+Console.WriteLine("Массив: ");
+foreach (int item in newArray)
+{
+    Console.Write(item + " ");
+
+}
+Console.WriteLine();
