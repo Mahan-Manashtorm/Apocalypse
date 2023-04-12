d6d40ed4064f3d22a859b9d09074d6299f526d82
Mahan-Manashtorm <mahan.manashtorm@gmail.com>
2023-04-12 04:26:53 +0300

    lab9

---
+++ real8/da/Class1.cs
@@ -0,0 +1,79 @@
+﻿namespace da
+{
+    public abstract class Muschroom
+    {
+        public abstract string Operation();
+    }
+
+
+    public class ConcreteMuschroom : Muschroom
+    {
+        public override string Operation()
+        {
+            return "Concrete Muschroom";
+        }
+    }
+
+    public abstract class Decorator : Muschroom
+    {
+        protected Muschroom _Muschroom;
+
+        public Decorator(Muschroom Muschroom)
+        {
+            this._Muschroom = Muschroom;
+        }
+
+        public void SetMuschroom(Muschroom Muschroom)
+        {
+            this._Muschroom = Muschroom;
+        }
+
+
+        public override string Operation()
+        {
+            if (this._Muschroom != null)
+            {
+                return this._Muschroom.Operation();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    public class Portobello : Decorator
+    {
+        public Portobello(Muschroom comp) : base(comp)
+        {
+        }
+
+
+        public override string Operation()
+        {
+            return $"Portobello({base.Operation()})";
+        }
+    }
+
+
+    public class Chanterelle : Decorator
+    {
+        public Chanterelle(Muschroom comp) : base(comp)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"Chanterelle({base.Operation()})";
+        }
+    }
+
+    public class Client
+    {
+
+        public void ClientCode(Muschroom Muschroom)
+        {
+            Console.WriteLine("RESULT: " + Muschroom.Operation());
+        }
+    }
+}
---
+++ real8/real8/Program.cs
@@ -0,0 +1,13 @@
+﻿using da;
+
+Client client = new Client();
+var simple = new ConcreteMuschroom();
+Console.WriteLine("Client: I get a simple Muschroom:");
+client.ClientCode(simple);
+Console.WriteLine();
+Portobello decorator1 = new Portobello(simple);
+Chanterelle decorator2 = new Chanterelle(simple);
+Console.WriteLine("Client: Now I've got a decorated Muschroom:");
+client.ClientCode(decorator1);
+client.ClientCode(decorator2);
+Console.ReadLine();
