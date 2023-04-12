c0ebcdbec281c1e729b56870a5beaa676b67321c
Mahan-Manashtorm <mahan.manashtorm@gmail.com>
2023-04-12 03:52:39 +0300

    lab4

---
+++ lab4/da/Class1.cs
@@ -0,0 +1,101 @@
+﻿namespace da
+{
+    public class Phone
+    {
+        public string Screen { get; set; }
+        public string Battery { get; set; }
+        public string Camera { get; set; }
+        public string Processor { get; set; }
+
+        public override string ToString()
+        {
+            return $"Screen: {Screen}, Battery: {Battery}, Camera: {Camera}, Processor: {Processor}";
+        }
+    }
+    public interface IPhoneBuilder
+    {
+        void SetScreen();
+        void SetBattery();
+        void SetCamera();
+        void SetProcessor();
+        Phone GetPhone();
+    }
+    public class BasicPhoneBuilder : IPhoneBuilder
+    {
+        private readonly Phone _phone = new Phone();
+
+        public void SetScreen()
+        {
+            _phone.Screen = "LCD";
+        }
+
+        public void SetBattery()
+        {
+            _phone.Battery = "2000mAh";
+        }
+
+        public void SetCamera()
+        {
+            _phone.Camera = "5MP";
+        }
+
+        public void SetProcessor()
+        {
+            _phone.Processor = "Snapdragon 200";
+        }
+
+        public Phone GetPhone()
+        {
+            return _phone;
+        }
+    }
+
+    public class PremiumPhoneBuilder : IPhoneBuilder
+    {
+        private readonly Phone _phone = new Phone();
+
+        public void SetScreen()
+        {
+            _phone.Screen = "AMOLED";
+        }
+
+        public void SetBattery()
+        {
+            _phone.Battery = "4000mAh";
+        }
+
+        public void SetCamera()
+        {
+            _phone.Camera = "12MP";
+        }
+
+        public void SetProcessor()
+        {
+            _phone.Processor = "Snapdragon 888";
+        }
+
+        public Phone GetPhone()
+        {
+            return _phone;
+        }
+    }
+    public class PhoneDirector
+    {
+        public void BuildBasicPhone(IPhoneBuilder builder)
+        {
+            builder.SetScreen();
+            builder.SetBattery();
+            builder.SetCamera();
+            builder.SetProcessor();
+        }
+
+        public void BuildPremiumPhone(IPhoneBuilder builder)
+        {
+            builder.SetScreen();
+            builder.SetBattery();
+            builder.SetCamera();
+            builder.SetProcessor();
+        }
+    }
+
+}
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,12 @@
+﻿using da;
+
+var director = new PhoneDirector();
+var basicBuilder = new BasicPhoneBuilder();
+var premiumBuilder = new PremiumPhoneBuilder();
+director.BuildBasicPhone(basicBuilder);
+var basicPhone = basicBuilder.GetPhone();
+Console.WriteLine($"Basic phone built:\n{basicPhone}\n");
+director.BuildPremiumPhone(premiumBuilder);
+var premiumPhone = premiumBuilder.GetPhone();
+Console.WriteLine($"Premium phone built:\n{premiumPhone}\n");
+Console.ReadLine();
