3d0093d8c75786aef5756e2fa9e72e71c6330fd4
Mahan-Manashtorm <mahan.manashtorm@gmail.com>
2023-04-12 03:52:07 +0300

    lab3

---
+++ Laba3/Laba3/Program.cs
@@ -0,0 +1,17 @@
+﻿using da;
+
+DocumentRepository documentRepository = DocumentRepository.Instance;
+Document document = new Document
+{
+    Id = 1,
+    Name = "Документ 1",
+    Content = "Содержимое документа 1"
+};
+documentRepository.AddDocument(document);
+List<Document> documents = documentRepository.GetDocuments();
+foreach (var doc in documents)
+{
+    Console.WriteLine($"ID: {doc.Id}, Name: {doc.Name}, Content: {doc.Content}");
+}
+
+Console.ReadLine();
---
+++ Laba3/da/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection.Metadata;
+
+namespace da
+{
+    public class DocumentRepository
+    {
+        private static DocumentRepository instance;
+        private List<Document> documents;
+
+        private DocumentRepository()
+        {
+            documents = new List<Document>();
+        }
+        public static DocumentRepository Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DocumentRepository();
+                }
+                return instance;
+            }
+        }
+        public void AddDocument(Document document)
+        {
+            documents.Add(document);
+        }
+
+        public List<Document> GetDocuments()
+        {
+            return documents;
+        }
+    }
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
