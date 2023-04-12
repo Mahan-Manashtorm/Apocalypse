5061e1b38809b260afbbf485f9c10816923c6dd3
Mahan-Manashtorm <mahan.manashtorm@gmail.com>
2023-04-12 04:16:09 +0300

    lab14

---
+++ laba14/da/Class1.cs
@@ -0,0 +1,58 @@
+﻿namespace da
+{
+    public class Sort
+    {
+        public static void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
+        public static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[minIndex];
+                    arr[minIndex] = temp;
+                }
+            }
+        }
+        public static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ laba14/laba14/Program.cs
@@ -0,0 +1,20 @@
+﻿using da;
+
+Console.WriteLine("Введите размер массива:");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+Random rnd = new Random();
+for (int i = 0; i < n; i++)
+{
+    arr[i] = rnd.Next(1000);
+}
+Task task1 = Task.Run(() => Sort.InsertionSort(arr));
+Task task2 = Task.Run(() => Sort.SelectionSort(arr));
+Task task3 = Task.Run(() => Sort.BubbleSort(arr));
+Task.WaitAll(task1, task2, task3);
+Console.WriteLine("Отсортированный массив:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(arr[i] + " ");
+}
+Console.WriteLine();
